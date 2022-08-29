a17ac0cdf8d94c92e553bcd9277882db7b1abb16
Shoney John <shoney.john@outlook.com>
2022-08-30 00:43:44 +0200

    async await task

--- Player.py
+++ Player.py
@@ -1,11 +1,39 @@
 class Player:
 
-    def __init__(self):
+    def __init__(self, name):
         self.myCards = []
+        self.discardPile = []
+        self.name = name
 
     def draw(self, card):
         self.myCards.append(card)
 
-    def print(self):       
-        for index in range(0, len(self.myCards)):
-            print(self.myCards[index].info())
+    def showTopCard(self):
+        if(len(self.myCards) > 0):
+            return self.myCards[0]
+        else:
+            return 'null'
+
+    def win(self, card):
+        self.discardPile.append(card)
+
+    def lose(self):
+        if(len(self.myCards) > 0):
+            self.myCards.pop(0)
+
+    def getRemainingCount(self):
+        return len(self.myCards) + len(self.discardPile)
+
+    def printPlaying(self):
+        self.totalCards = len(self.myCards) + len(self.discardPile)
+        s = '{} ({} cards):{}'.format(
+            self.name, self.totalCards, self.showTopCard().get())
+        print(s)
+
+    def printRoundWinner(self):
+
+        print('{} wins this round'.format(self.name))
+
+    def printWinner(self):
+
+        print('{} wins the game'.format(self.name))
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import asyncio
 from random import randint
 
 from Card import Card
 from Player import Player
 
 
-def shuffle(deck, n):
+async def shuffle(deck, n):
+    print("Shuffling the deck")
     # Start from the last element and swap one by one. We don't
     # need to run for the first element that's why i > 0
     for i in range(n-1, 0, -1):
@@ -13,46 +15,101 @@ def shuffle(deck, n):
 
     # Swap arr[i] with the element at random index
         deck[i], deck[j] = deck[j], deck[i]
+        await asyncio.sleep(0.5)
+
+    return deck
 
 
-def main():
-    # task 1 create a deck of card showing number 1 to 10
-    # each number is in the deck 4 times
+async def createDeck():
+    print("Creating deck of cards")
     deck = []
     for cardNum in range(1, 11):
         deck.append(Card(cardNum))
+        deck.append(Card(cardNum))
+        deck.append(Card(cardNum))
+        deck.append(Card(cardNum))
 
+    await asyncio.sleep(5)
+    return deck
+
+
+async def drawCardForPlayer(player1, player2, deck):
+    print("Distributing cards to player")
     n = len(deck)
-    shuffle(deck, n)
-
-
-# each players receives 20 cards from shuffled.
-
-    player1 = Player()
-
-    player2 = Player()
-
     tempdeck = deck
-
-
-    print ("deck info")
-    for obj in deck:
-        print (obj.info())
-
-   
     while n > 0:
         player1.draw(tempdeck[0])
         tempdeck.pop(0)
         player2.draw(tempdeck[0])
         tempdeck.pop(0)
         n = n-2
-        
-    print ("player one info")
-    player1.print()
-
-    print ("player two info")
-    player2.print()
+    await asyncio.sleep(5)
 
 
-main()
+async def play(player1, player2):
+    if hasattr(player1.showTopCard(), 'get'):
+        playCard1 = player1.showTopCard().get()
+    else:
+        player2.printWinner()
+
+    if hasattr(player2.showTopCard(), 'get'):
+        playCard2 = player2.showTopCard().get()
+    else:
+        player1.printWinner()
+
+    if(playCard1 > playCard2):
+        player1.win(player2.showTopCard())
+        player2.lose()
+        player1.printRoundWinner()
+
+    elif(playCard1 < playCard2):
+        player2.win(player1.showTopCard())
+        player1.lose()
+        player2.printRoundWinner()
+    elif(playCard1 == playCard2):
+        onTie()
+
+    await asyncio.sleep(2)
+
+
+async def onTie():
+    print("To do")
+
+
+async def main():
+    # task 1 create a deck of card showing number 1 to 10
+    # each number is in the deck 4 times
+    task1 = asyncio.create_task(createDeck())
+    newDeck = await task1
+    n = len(newDeck)
+    task2 = asyncio.create_task(shuffle(newDeck, n))
+    shuffledDeck = await task2
+
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    task3 = asyncio.create_task(
+        drawCardForPlayer(player1, player2, shuffledDeck))
+    await task3
+
+    while player1.showTopCard() != 'null' or player2.showTopCard() != 'null':
+
+        if(player1.showTopCard() != 'null' and player2.showTopCard() != 'null'):
+            player1.printPlaying()
+            player2.printPlaying()
+
+        task4 = asyncio.create_task(play(player1, player2))
+
+        value =await task4
+
+        print (value)
+
+        # print ('player one cards {}'.format(player1.showTopCard()))
+        # print ('player two cards {}'.format(player2.showTopCard()))
+
+        # else:
+        #     print (player1.showTopCard())
+
+
+asyncio.run(main())
 # Draw cards.
